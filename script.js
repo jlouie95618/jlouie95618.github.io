a447f71981c2621977d445cd51b18f6f14180122
John Louie <jlouie95618@gmail.com>
2015-01-07 10:56:21 -0800

    lots more work to do, need to think of a redesign...

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-// Standard jQuery:
-
-// $(document).ready(function(){
-//     $("div").slideDown("slow")
-// });
-
-// $(document).ready(function() {
-//     $('div').mouseenter( function() {
-//         $('div').fadeTo("fast", 1);
-//     });
-    
-//     $('div').mouseleave( function() {
-//         $('div').fadeTo('fast', 0.5);
-//     });
-    
-// });
-
-// Structure of jQuery functions:
-
-// function(input1, input2, etc) {
-//     Do a thing
-//     Do another thing
-//     Do yet another thing!
-// }
+// Javascript/jQuery document to accompany index.html
 
 var main = function() {
      $('.tutoring').fadeOut('slow');
